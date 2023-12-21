c48493e90acdcf2de0e6a4393b48b6fd5b2c5b46
zineb abercha <ZINEB.ABERCHA@um6p.ma>
2023-12-21 20:50:08 +0100

    folder

---
+++ classes/doctor.php
@@ -0,0 +1,14 @@
+<?php
+
+class Doctor {
+    public $doctorID;
+    public $username;
+    public $password;
+    public $email;
+    public $phoneNumber;
+    public $address;
+    public $CIN;
+}
+
+
+?>
