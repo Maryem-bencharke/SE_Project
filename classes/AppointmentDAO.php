7ca35b39ffa1361bdf3e1718ea3221d626a956bc
Maryem BEN-CHARKE <72795298+Maryem-bencharke@users.noreply.github.com>
2023-12-22 12:06:56 +0100

    AppointmentDAO.php

--- classes/AppointmentDAO.php
+++ classes/AppointmentDAO.php
@@ -1,69 +1,85 @@
 <?php
-require_once 'Db.php'; 
 
-class AppointmentDAO extends AbstractDAO {
-    private static $instance = null;
+ require 'db/db.php';
 
-    // Private constructor for Singleton
-    private function __construct() {
-        parent::__construct(); 
+interface AppointmentDAO {
+    public function createAppointment($appointment);
+    public function readAppointment($appointmentId);
+    public function updateAppointment($appointment);
+    public function deleteAppointment($appointmentId);
+}
+class Appointment {
+    private $_appointmentId;
+    private $_appointmentDate;
+    private $_appointmentStatus;
+
+    public function __construct($appointmentId, $appointmentDate, $appointmentStatus) {
+        $this->_appointmentId = $appointmentId;
+        $this->_appointmentDate = $appointmentDate;
+        $this->_appointmentStatus = $appointmentStatus;
     }
 
-    // Singleton getInstance method
-    public static function getInstance() {
-        if (self::$instance == null) {
-            self::$instance = new AppointmentDAO();
-        }
-        return self::$instance;
+    // Getters
+    public function getAppointmentId() {
+        return $this->_appointmentId;
     }
 
-    // Create an appointment
-    public function createAppointment($appointmentDetails) {
-        $sql = "INSERT INTO Appointment (PatientID, DoctorID, NurseID, AppointmentDate, Status) VALUES (:PatientID, :DoctorID, :NurseID, :AppointmentDate, :Status)";
+    public function getAppointmentDate() {
+        return $this->_appointmentDate;
+    }
+
+    public function getAppointmentStatus() {
+        return $this->_appointmentStatus;
+    }
+
+    // Setters
+    public function setAppointmentId($appointmentId) {
+        $this->_appointmentId = $appointmentId;
+    }
+
+    public function setAppointmentDate($appointmentDate) {
+        $this->_appointmentDate = $appointmentDate;
+    }
+
+    public function setAppointmentStatus($appointmentStatus) {
+        $this->_appointmentStatus = $appointmentStatus;
+    }
+}
+class AppointmentDAOImpl extends AbstractDAO implements AppointmentDAO {
+    
+    public function createAppointment($appointment) {
+        $sql = "INSERT INTO Appointment (appointmentDate, appointmentStatus) VALUES (?, ?)";
         $stmt = $this->_connection->prepare($sql);
-
-        // Bind parameters from $appointmentDetails
-        $stmt->bindValue(':PatientID', $appointmentDetails['PatientID']);
-        $stmt->bindValue(':DoctorID', $appointmentDetails['DoctorID']);
-        $stmt->bindValue(':NurseID', $appointmentDetails['NurseID']);
-        $stmt->bindValue(':AppointmentDate', $appointmentDetails['AppointmentDate']);
-        $stmt->bindValue(':Status', $appointmentDetails['Status']);
-
-        $stmt->execute();
+        $stmt->execute([
+            $appointment->getAppointmentDate(),
+            $appointment->getAppointmentStatus()
+        ]);
     }
 
-    // Read an appointment
     public function readAppointment($appointmentId) {
-        $sql = "SELECT * FROM Appointment WHERE AppointmentID = :AppointmentID";
+        $sql = "SELECT * FROM Appointment WHERE appointmentId = ?";
         $stmt = $this->_connection->prepare($sql);
-        $stmt->bindValue(':AppointmentID', $appointmentId);
-        $stmt->execute();
-
+        $stmt->execute([$appointmentId]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    // Update an appointment
-    public function updateAppointment($appointmentData) {
-        $sql = "UPDATE Appointment SET PatientID = ?, DoctorID = ?, NurseID = ?, AppointmentDate = ?, Status = ? WHERE AppointmentID = ?";
+    public function updateAppointment($appointment) {
+        $sql = "UPDATE Appointment SET appointmentDate = ?, appointmentStatus = ? WHERE appointmentId = ?";
         $stmt = $this->_connection->prepare($sql);
-    
         $stmt->execute([
-            $appointmentData['PatientID'],
-            $appointmentData['DoctorID'],
-            $appointmentData['NurseID'],
-            $appointmentData['AppointmentDate'],
-            $appointmentData['Status'],
-            $appointmentData['AppointmentID']
+            $appointment->getAppointmentDate(),
+            $appointment->getAppointmentStatus(),
+            $appointment->getAppointmentId()
         ]);
     }
-    
-    // Delete an appointment
-    public function deleteAppointment($appointmentId) {
-        $sql = "DELETE FROM Appointment WHERE AppointmentID = :AppointmentID";
-        $stmt = $this->_connection->prepare($sql);
-        $stmt->bindValue(':AppointmentID', $appointmentId);
 
-        $stmt->execute();
+    public function deleteAppointment($appointmentId) {
+        $sql = "DELETE FROM Appointment WHERE appointmentId = ?";
+        $stmt = $this->_connection->prepare($sql);
+        $stmt->execute([$appointmentId]);
     }
 }
-?>
+
+
+
+?> 
