14940c32fe1a7a8a4883dfe2eecacee903b82118
zineb abercha <ZINEB.ABERCHA@um6p.ma>
2023-12-23 14:45:16 +0100

    added doctor

--- classes/DoctorDAO.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*interface DoctorDAO {
-    public function createDoctor($doctor);
-    public function findDoctor($cin);
-    public function updateDoctor($doctor, $cin);
-    public function deleteDoctor($cin);
-}
-?>*/
