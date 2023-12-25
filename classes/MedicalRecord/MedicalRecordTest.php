c4a42795b12065848183349ed735235499b3294f
Maryem BEN-CHARKE <72795298+Maryem-bencharke@users.noreply.github.com>
2023-12-25 15:38:00 +0100

    MedicalRecordTest.php

--- classes/MedicalRecord/MedicalRecordTest.php
+++ classes/MedicalRecord/MedicalRecordTest.php
@@ -1,42 +1,29 @@
 <?php
-require_once 'MedicalRecordDAOImpl.php';
+
 require_once 'MedicalRecord.php';
+require_once 'MedicalRecordDAOImpl.php';
 
-// Create a new MedicalRecord instance
-$medicalRecord = new MedicalRecord(
-    null, // RecordID is auto-increment, so pass null or don't include it in the constructor
-    1,    // PatientID
-    2,    // DoctorID
-    3,    // NurseID
-    '2023-01-01', // DateCreated
-    'Sample Medical Record', // Record
-    'Treatment Plan',        // TreatmentPlan
-    'Test Results',          // TestResults
-    null  // ImageData (binary data or null)
-);
-
-// Create a new medical record in the database
+// Create an instance of MedicalRecordDAOImpl
 $medicalRecordDAO = new MedicalRecordDAOImpl();
+
+// Example data for creating a new medical record
+$medicalRecord = new MedicalRecord(null, 1, 1, 1, '2023-01-01', 'Example medical record', 'Example treatment plan', 'Example test results', null);
+
+// Test createMedicalRecord
 $medicalRecordDAO->createMedicalRecord($medicalRecord);
-echo "\nMedical record added\n";
+echo "Medical record created.\n";
 
-// Assuming you retrieve the RecordID after insertion
-// This depends on your implementation details on how you handle auto-incremented IDs post-insertion
-$recordId = 1; // Replace with the actual ID or retrieval method
+// Test readMedicalRecord (use an existing RecordID from your database)
+$recordId = 1; // Example RecordID
+$readMedicalRecord = $medicalRecordDAO->readMedicalRecord($recordId);
+echo "Read Medical Record:\n";
+print_r($readMedicalRecord);
 
-// Read a medical record from the database
-$retrievedRecord = $medicalRecordDAO->readMedicalRecord($recordId);
-echo "Retrieved Record: ";
-print_r($retrievedRecord);
-echo "\n";
-
-$medicalRecord->setRecordId($recordId); // Set the record ID to identify which record to update
+// Test updateMedicalRecord (use an existing RecordID from your database)
+$medicalRecord->setRecordId($recordId);
 $medicalRecordDAO->updateMedicalRecord($medicalRecord);
-echo "\nMedical record updated\n";
+echo "Medical record updated.\n";
 
-// Delete a medical record from the database
+// Test deleteMedicalRecord (use an existing RecordID from your database)
 $medicalRecordDAO->deleteMedicalRecord($recordId);
-echo "\nMedical record deleted\n";
-
-
-?>
+echo "Medical record deleted.\n";
