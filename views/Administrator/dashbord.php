93f30990d6c69b69a604f0c61ed0742e32f635d4
Maryem <maryem.ben-charke@um6p.ma>
2023-12-31 01:18:12 +0100

    the dashbord of administrator and managing the doctors

---
+++ views/Administrator/dashbord.php
@@ -0,0 +1,79 @@
+<?php
+// check if the user is logged in
+session_start();
+if(!isset($_SESSION["userID"])){
+    header("Location: ../../index.php");
+    exit();
+}
+// check if the user is an administrator
+if($_SESSION["role"] != "administrator"){
+    header("Location: ../../index.php");
+    exit();
+}
+
+// Include necessary files
+require_once '../../db/db.php';
+require_once '../../classes/AdministratorDAOImpl.php';
+
+$adminDao = new AdministratorDAOImpl();
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>Administrator Dashboard - Hospital</title>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+            <div class="container">
+                <a class="navbar-brand text12">Hospital management system</a>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./dashboard.php">Dashboard</a>
+                    </li>
+                    </ul>
+                </div>
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#logout">Logout</button>
+                <div class="modal fade" id="logout" tabindex="-1" aria-labelledby="logoutLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="logoutLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <i class="fa fa-question-circle"></i> Are you sure you want to log-off?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <!-- <button type="button" class="btn btn-danger">Logout</button> -->
+                            <a href="../../logout.php" class="btn btn-danger btn-block">Logout</a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </nav>
+        </header>
+    <main class="container">
+        <h3>Welcome, <?php echo $_SESSION["USER_NAME"]; ?></h3>
+        <div class="dashboard-section">
+            <h4>Manage Users</h4>
+            <a href="doctors.php" class="btn btn-primary">Manage Doctors</a>
+            <a href="nurses.php" class="btn btn-primary">Manage Nurses</a>
+            <a href="patients.php" class="btn btn-primary">Manage Administrators</a>
+        </div>
+        <!-- <div class="dashboard-section">
+            <h4>Reports</h4>
+            <a href="generate_reports.php" class="btn btn-primary">Generate Reports</a>
+        </div> -->
+    </main>
+</body>
+</html>
